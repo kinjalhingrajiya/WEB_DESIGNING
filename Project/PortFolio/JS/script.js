f37018bbb21632a2bb51282473992a28af6ead10
kinjalhingrajiya <125260636+kinjalhingrajiya@users.noreply.github.com>
2023-05-15 11:49:02 +0530

    j

---
+++ Project/PortFolio/JS/script.js
@@ -0,0 +1,35 @@
+body_tag = document.getElementById('body1');
+header_tag = document.getElementById('header');
+
+toggle2 = 1;
+
+function bodycolor() {
+
+    if (toggle2 == 1) {
+        body_tag.style.background = "#000"
+        body_tag.style.color = "#fff"
+        header_tag.style.background = "#000";
+        header_tag.style.color = "#fff";
+        header_tag.style.borderRight = "0.5px solid gray";
+        toggle2 = 0;
+    } else if (toggle2 == 0) {
+        body_tag.style.background = "#fff"
+        header_tag.style.background = "#fff";
+        body_tag.style.color = "#000"
+        header_tag.style.borderRight = "none";
+        toggle2 = 1;
+    }
+}
+
+function menu1() {
+    // header_tag.classList.add('menuright');
+
+
+    if (toggle2 == 1) {
+        header_tag.style.left = "0px";
+        toggle2--;
+    } else if (toggle2 == 0) {
+        header_tag.style.left = "-300px";
+        toggle2++;
+    }
+}
